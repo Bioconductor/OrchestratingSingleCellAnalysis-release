4ac15f0fa1cee5cf1fda911088a7edcdf4428826
Robert A. Amezquita <2438701+robertamezquita@users.noreply.github.com>
2019-02-07 10:35:01 -0800

    continuing edits to intro

--- book-raw-v2/_rfiles/clustering.R
+++ book-raw-v2/_rfiles/clustering.R
@@ -3,6 +3,8 @@
 library(TENxPBMCData)
 library(scater)
 library(scran)
+library(SC3)
+library(clusterExperiment)
 
 ## Load dataset
 sce <- TENxPBMCData(dataset = "pbmc3k")
@@ -41,25 +43,49 @@ sig_genes <- rownames(dec[dec$FDR < 0.00001, ])
 
 
 ## Dimension reduction ---------------------------------
+sce <- runPCA(sce, ncomponents = 20, method = "irlba", feature_set = sig_genes)
 sce <- runTSNE(sce, perplexity = 15, feature_set = sig_genes)
 
+plotReducedDim(sce, "PCA", colour_by = "log10_total_counts")
 plotReducedDim(sce, "TSNE", colour_by = "log10_total_counts")
 
 
-## Performing clustering -------------------------------
-library(SC3)
-
+## Performing clustering: SC3 -------------------------------
 rowData(sce)$feature_symbol <- rownames(sce)
 
 sce_sc3 <- sc3(sce[sig_genes, ], ks = 2:4, k_estimator = TRUE)
 
+## Basic code:
+g2 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_2_clusters")
+g3 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_3_clusters")
+g4 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_4_clusters")
+patchwork::wrap_plots(g2, g3, g4, widths = 1, heights = 1, ncol = 1)
+
+## ## Alternate code: Plot the various clusters programmatically
+## cols <- paste0("sc3_", 2:4, "_clusters")
+## g <- lapply(cols, function(sce, col) {
+##     plotReducedDim(sce, use_dimred = "TSNE", colour_by = col) +
+##         theme(legend.position = "none")
+## }, sce = sce_sc3)
+## patchwork::wrap_plots(g, widths = 1, heights = 1)
+
+cols <- paste0("sc3_", 2:4, "_clusters") # "sc3_*_clusters"
+
+sc3_plot_consensus(sce_sc3, k = 3, show_pdata = cols)
+
+## Performing clustering: clusterExperiment -------------------------------
+
+## This takes a while..>15 mins on a laptop for ~2500 cells
+rsec <- RSEC(sce[sig_genes, ], reduceMethod = "PCA", nReducedDims = 20,
+            alphas = 0.1,
+            consensusMinSize = 100, k0s = 2:4)
+
+
+plotCoClustering(rsec, whichClusters = c("mergeClusters", "makeConsensus"))
 
 
 
-
-
-
-
+save(sce, sce_sc3, rsec, file = "_rfiles/_data/clustering.RData")
 
 
 
