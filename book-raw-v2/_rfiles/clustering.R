13fcfd962282c17823811d43621dab4ceeb37393
Robert A. Amezquita <2438701+robertamezquita@users.noreply.github.com>
2019-02-28 13:07:28 -0800

    book filled in more with preamble and workflow 1
    Former-commit-id: 76d6cffc584fb6b88b27da526dbe0b2b6ad093f2

--- book-raw-v2/_rfiles/clustering.R
+++ book-raw-v2/_rfiles/clustering.R
@@ -1,10 +1,12 @@
 ## Clustering workflow R file
-
 library(TENxPBMCData)
 library(scater)
 library(scran)
 library(SC3)
 library(clusterExperiment)
+
+## Load all results in
+## sce <- readRDS('_rfiles/_data/clustering_sce.rds')
 
 ## Load dataset
 sce <- TENxPBMCData(dataset = "pbmc3k")
@@ -41,6 +43,9 @@ fit <- trendVar(sce, parametric=TRUE, use.spikes = FALSE)
 dec <- decomposeVar(sce, fit)
 sig_genes <- rownames(dec[dec$FDR < 0.00001, ])
 
+## remove uninteresting/unknown genes
+sig_genes <- sig_genes[!grepl('RP[0-9]|^MT|[0-9][0-9][0-9][0-9]', sig_genes)]
+
 
 ## Dimension reduction ---------------------------------
 sce <- runPCA(sce, ncomponents = 20, method = "irlba", feature_set = sig_genes)
@@ -53,7 +58,9 @@ plotReducedDim(sce, "TSNE", colour_by = "log10_total_counts")
 ## Performing clustering: SC3 -------------------------------
 rowData(sce)$feature_symbol <- rownames(sce)
 
-sce_sc3 <- sc3(sce[sig_genes, ], ks = 2:4, k_estimator = TRUE)
+sce <- sc3(sce[sig_genes, ], ks = 2:4, k_estimator = TRUE)
+
+sc3_cols <- as.data.frame(colData(sce_sc3))[, c("sc3_2_clusters", "sc3_3_clusters", "sc3_4_clusters")]
 
 ## Basic code:
 g2 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_2_clusters")
@@ -61,30 +68,61 @@ g3 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_3_clusters")
 g4 <- plotReducedDim(sce_sc3, use_dimred = "TSNE", colour_by = "sc3_4_clusters")
 patchwork::wrap_plots(g2, g3, g4, widths = 1, heights = 1, ncol = 1)
 
-## ## Alternate code: Plot the various clusters programmatically
-## cols <- paste0("sc3_", 2:4, "_clusters")
-## g <- lapply(cols, function(sce, col) {
-##     plotReducedDim(sce, use_dimred = "TSNE", colour_by = col) +
-##         theme(legend.position = "none")
-## }, sce = sce_sc3)
-## patchwork::wrap_plots(g, widths = 1, heights = 1)
-
-cols <- paste0("sc3_", 2:4, "_clusters") # "sc3_*_clusters"
-
-sc3_plot_consensus(sce_sc3, k = 3, show_pdata = cols)
 
 ## Performing clustering: clusterExperiment -------------------------------
 
 ## This takes a while..>15 mins on a laptop for ~2500 cells
-rsec <- RSEC(sce[sig_genes, ], reduceMethod = "PCA", nReducedDims = 20,
-            alphas = 0.1,
-            consensusMinSize = 100, k0s = 2:4)
+rsec <- RSEC(sce[sig_genes, ],
+            reduceMethod = "PCA", nReducedDims = 20,
+            alphas = c(0.1, 0.3, 0.5),
+            consensusMinSize = 50, k0s = 3)
 
-plotCoClustering(rsec, whichClusters = c("mergeClusters", "makeConsensus"))
+## Extract cluster matrix
+clust_mat <- as.data.frame(clusterMatrix(rsec))
+rsec_clusters <- as.character(clust_mat$mergeClusters)
+    
+## Append to colData
+colData(sce)$rsec_clusters <- rsec_clusters
+
+## Basic code:
+plotReducedDim(sce, use_dimred = "TSNE", colour_by = "rsec_clusters")
+
+
+## Save results ------------------------------------------------------------
+saveRDS(sce, file = "_rfiles/_data/clustering_sce.rds", compress = 'xz')
 
 
 
-save(sce, sce_sc3, rsec, file = "_rfiles/_data/clustering.RData", compress = 'xz')
+## Figures for review iSEE figure -------------------------------------------------------------
+## base <- '/Users/ramezqui/Dropbox (Gottardo Lab)/GoTeam/Members/Rob/gottardo_bioc-review/'
 
+## ## reduced dim - with clusters
+## png(paste0(base, 'example_tsne.png'),
+##     height = 1600, width = 3200, res = 600)
+## plotTSNE(sce, colour_by = "sc3_3_clusters")
+## dev.off()
 
+## ## Feature data - base
+## png(paste0(base, 'example_violins.png'),
+##     height = 1200, width = 2400, res = 600)
+## plotExpression(sce, c('CD79A', 'LYZ', 'CD3E'), colour_by = "total_counts")
+## dev.off()
 
+## ## Feature data - with clusters
+## png(paste0(base, 'example_violins_facet-clusters.png'),
+##     height = 1200, width = 2400, res = 600)
+## plotExpression(sce, c('CD79A', 'LYZ', 'CD3E'), x = "sc3_3_clusters",
+##                colour_by = "sc3_3_clusters", ncol = 3)
+## dev.off()
+
+## ## coldata plot
+## png(paste0(base, 'example_coldata.png'),
+##     height = 1200, width = 1600, res = 600)
+## plotColData(sce, y = "total_features_by_counts", x = "Individual")
+## dev.off()
+
+## png(paste0(base, 'example_coldata_facet-clusters.png'),
+##     height = 1200, width = 2400, res = 600)
+## plotColData(sce, y = "total_counts", x = "sc3_3_clusters",
+##             colour_by = "sc3_3_clusters")
+## dev.off()
