932e3f82b1c73aaf67b16c56d50c6c1ff42b0a56
robertamezquita <2438701+robertamezquita@users.noreply.github.com>
2019-08-02 10:19:30 -0700

    edited cron to use ssh push in lieu of token/https

--- _cron.sh
+++ _cron.sh
@@ -5,19 +5,20 @@
 ## FHCRC specific modules -----------------------------
 source /app/Lmod/lmod/lmod/init/bash
 module use /app/easybuild/modules/all
-source ~/.github_pat   # github access token env var
+## source ~/.github_pat   # github access token only needed for https remotes
 ml R/3.6.1-foss-2016b-fh1
 ml pandoc
+
+## Cron saver
 mkdir -p ~/cronjobs/builds ~/cronjobs/logs
 cd ~/cronjobs/builds
 
-
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
 
 ## Cron job script ------------------------------------
 echo "Build time is $(date) ..."
-
-TMPDIR=$(mktemp -d)
-cd $TMPDIR
+echo "Writing to ${TMPDIR} ..."
 
 REPO=OrchestratingSingleCellAnalysis
 
@@ -26,7 +27,9 @@ if [ -e ${REPO} ]
 then
     rm -rf ${REPO}
 fi
-git clone https://github.com/Bioconductor/${REPO}
+## git clone https://github.com/Bioconductor/${REPO}.git # for token method
+git clone git@github.com:Bioconductor/${REPO}.git
+
 
 cd ${REPO}
 git pull
@@ -47,6 +50,7 @@ bash _build.sh
 git add docs/
 git commit -m "Rebuilt book."
 
-# Make a personal access token.
-TOKEN=${GITHUB_PAT}
-git push https://${TOKEN}:x-oauth-basic@github.com/Bioconductor/${REPO} master
+# Make a personal access tokenized push
+## TOKEN=${GITHUB_PAT}  ## required for remote to use https
+## git push https://${TOKEN}:x-oauth-basic@github.com/Bioconductor/${REPO} master
+git push # only works if pushing via ssh
