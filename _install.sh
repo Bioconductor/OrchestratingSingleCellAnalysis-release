963a30c6fbd758d9a57ac616aff2782376aeb545
robertamezquita <2438701+robertamezquita@users.noreply.github.com>
2019-09-04 14:03:13 -0700

    tweaks to install script to check BiocManager::valid and add manual pkgs

--- _install.sh
+++ _install.sh
@@ -4,8 +4,39 @@
 
 base=OSCABase
 
+LIBLOC=/home/ramezqui/R/x86_64-pc-linux-gnu-library/3.6
+
+
 ###################################################
-## Write the DESCRIPTION file automagically 
+## Install/update all libraries
+
+## Dependencies
+PKGS=$(grep --text -h -r "^library(" ${base} | awk '{FS=" "}{print $1}' | sort | uniq | sed 's/library(/\"/g' | sed 's/)/\",/g' | tr -d '\n\r')
+CMD=$(echo "BiocManager::install(c(${PKGS}), lib = '$LIBLOC', ask = FALSE, update = TRUE)") ## add pkg to end line properly
+
+## Remove any LOCK instances
+rm -Rf ~/R/*/*/*LOCK*
+
+## Prereq packages
+R --no-save --slave -e "install.packages(c('devtools', 'BiocManager', 'knitr', 'bookdown'), lib = '$LIBLOC')"
+
+## Install dependencies
+R --no-save --slave -e "${CMD}" 
+
+## Supplementary packages (manually added)
+R --no-save --slave -e "BiocManager::install('GO.db', lib = '$LIBLOC')"
+
+
+## Remote packages (manually added)
+R --no-save --slave -e "devtools::install_github('stephenturner/msigdf', lib = '$LIBLOC')"
+R --no-save --slave -e "devtools::install_github('LTLA/SingleR', lib = '$LIBLOC')"
+
+## Check that Bioc pkgs are valid, else fix it!
+R --no-save --slave -e "valid <- BiocManager::valid('$LIBLOC'); if (identical(valid, TRUE)) { quit('no') } else { BiocManager::install(rownames(valid$out_of_date), lib = '$LIBLOC') }"
+
+
+###################################################
+## Bonus: Write the DESCRIPTION file automagically 
 
 echo "Package: OrchestratingSingleCellAnalysis" > DESCRIPTION
 echo "Title: Orchestrating Single Cell Analysis with Bioconductor" >> DESCRIPTION
@@ -24,23 +55,3 @@ grep --text -h -r "^library(" ${base} | awk '{FS=" "}{print $1}' | sort | uniq |
 echo "    BiocManager" >> DESCRIPTION
 echo "    devtools" >> DESCRIPTION
 echo "    bookdown" >> DESCRIPTION
-
-
-###################################################
-## Install/update all libraries
-
-## Dependencies
-PKGS=$(grep --text -h -r "^library(" ${base} | awk '{FS=" "}{print $1}' | sort | uniq | sed 's/library(/\"/g' | sed 's/)/\",/g' | tr -d '\n\r')
-CMD=$(echo "BiocManager::install(c(${PKGS} 'knitr'), ask = FALSE, update = TRUE)") ## add pkg to end line properly
-
-## Remove any LOCK instances
-rm -Rf ~/R/*/*/*LOCK*
-
-## Prereq packages
-R --no-save --slave -e "install.packages('BiocManager'); install.packages('devtools'); install.packages('bookdown')"
-
-## Install dependencies
-R --no-save --slave -e "${CMD}" 
-
-## Remote packages (manually added)
-# R --no-save --slave -e "devtools::install_github('stephenturner/msigdf'); devtools::install_github('LTLA/SingleR')"
